e45b8695950937b96f90aa8e48b4da5870460e45
SlavAbent <slavabent2015@gmail.com>
2021-03-24 12:04:18 +0500

    create cards

--- w_work/js/main.js
+++ w_work/js/main.js
@@ -11,8 +11,7 @@ const mySwiper = new Swiper('.swiper-container', {
 //Cart
 
 const buttonCart = document.querySelector('.button-cart'),
-	  modalCart = document.querySelector('#modal-cart'),
-	  modalClose = document.querySelector('.modal-close')
+	  modalCart = document.querySelector('#modal-cart')
 
 const openModal = function() {
 	modalCart.classList.add('show')
@@ -22,16 +21,21 @@ const closeModal = function() {
 }
 
 buttonCart.addEventListener('click', openModal)
-modalClose.addEventListener('click', closeModal)
 
+modalCart.addEventListener('click', (e) => {
+	const target = e.target
+	if(e.target.classList.contains('overlay') || target.classList.contains('modal-close')){
+		closeModal()
+	}
+})
 //scroll smooth
 {
 	const scrollLinks = document.querySelectorAll('a.scroll-link')
 
-	for(let i = 0; i < scrollLinks.length; i++){
-		scrollLinks[i].addEventListener('click', function(e){
+	for(const scrollLink of scrollLinks){
+		scrollLink.addEventListener('click', function(e){
 			e.preventDefault()
-			const id = scrollLinks[i].getAttribute('href')
+			const id = scrollLink.getAttribute('href')
 			document.querySelector(id).scrollIntoView({
 				behavior: 'smooth',
 				block: 'start' 
@@ -39,3 +43,67 @@ modalClose.addEventListener('click', closeModal)
 		})
 	}
 }
+
+
+// goods
+
+const more = document.querySelector('.more')
+const navigationLink = document.querySelectorAll('.navigation-link')
+const longGoodsList = document.querySelector('.long-goods-list')
+
+const getGoods = async function() {
+	const result = await fetch('db/db.json')
+	if(!result.ok){
+		throw 'Ошибка' + result.status
+	}
+	return await result.json()
+}
+
+const createCard = function({ label, name, img, description, id, price }){
+	const card = document.createElement('div')
+	card.className = 'col-lg-3 col-sm-6'
+
+	card.innerHTML = `
+	<div class="goods-card">
+		${label ? `<span class="label">${label}</span>`: ''}
+		<img src="db/${img}" alt="${name}" class="goods-image">
+		<h3 class="goods-title">${name}</h3>
+		<p class="goods-description">${description}</p>
+		<button class="button goods-card-btn add-to-cart" data-id="${id}">
+			<span class="button-price">$${price}</span>
+		</button>
+	</div>`
+	return card
+}
+
+const renderCards = function(data){
+	longGoodsList.textContent = ''
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards)
+	document.body.classList.add('show-goods')
+}
+
+more.addEventListener('click', function(e){
+	e.preventDefault()
+	getGoods().then(renderCards)
+})
+
+const filterCards = function(field, value){
+	getGoods()
+		.then(function(data){
+			const filteredGoods = data.filter(function(good) {
+				return good[field] === value
+			})
+			return filteredGoods
+		})
+		.then(renderCards);
+} 
+
+navigationLink.forEach(function(link){
+	link.addEventListener('click', function(e){
+		e.preventDefault()
+		const field = link.dataset.field
+		const value = link.textContent;
+		filterCards(field, value)
+	});
+})
